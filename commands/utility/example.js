2fa51091c66119a7b3123db5e3422b54afb276cd
csc32 <sanzonettycar@gmail.com>
2024-01-04 13:20:58 -0400

    add example generator command

---
+++ commands/utility/example.js
@@ -0,0 +1,26 @@
+import { CohereClient } from "cohere-ai";
+import { SlashCommandBuilder } from "discord.js";
+
+const cohere = new CohereClient({
+  token: process.env.COHERE_API_KEY
+})
+
+const data = new SlashCommandBuilder()
+  .setName('example')
+  .setDescription('Generate an example about a specific topic').addStringOption(option => option.setRequired(true).setName("topic").setDescription("The topic of the example"))
+async function execute(interaction) {
+  try {
+    await interaction.deferReply();
+    const res = await cohere.generate({
+      prompt: `Please generate just one example of ${interaction.options.getString('topic')}`,
+      temperature: 1
+    });
+    await interaction.editReply({ content: res.generations[0].text });
+  } catch (error) {
+    await interaction.editReply({ content: `There was an error: ${error}`, ephemeral: true });
+  }
+}
+export default {
+  data,
+  execute
+}
